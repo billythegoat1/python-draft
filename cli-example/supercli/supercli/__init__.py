1fde91896be83bd976bf57df9d151dee411ee807
billythegoat1 <nitungabilly@gmail.com>
2025-07-08 19:42:13 +0300

    Added a little

--- cli-example/supercli/supercli/__init__.py
+++ cli-example/supercli/supercli/__init__.py
@@ -1,6 +1,9 @@
 import click
 
 @click.command()
-@click.option('--name', '-n', help="The name of the person to say hello to!", required=True)
-def supercli(name):
-    print("Hello, "+name)
+@click.option('--name', '-n', help="The name of the person to say hello to!", required=True, prompt="Your name, please ? ")
+@click.argument('number')
+def supercli(name, number):
+    for _ in range((int(number))):
+        print("A super important text for "+name+":\n\n\tYou have been a "
+        "great person to talk with ...")
